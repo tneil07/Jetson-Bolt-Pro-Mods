32df7339a5f2b2a5d4aabb906bf88d777dcdaa7f
tneil07 <47345020+tneil07@users.noreply.github.com>
2024-08-07 17:34:23 -0230

    Update JetsonSpeedHack.ino

--- JetsonSpeedHack.ino
+++ JetsonSpeedHack.ino
@@ -30,7 +30,7 @@
  * 
  * @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
  */
-#define TARGET_SPEED_MPH 19
+#define TARGET_SPEED_MPH 20
 
 /*
  * @@@@@@@@@@@@@@ TO INCREASE ABOVE 19 MPH, PLEASE READ ALL OF THE BELOW @@@@@@@@@@@@@@
@@ -52,7 +52,7 @@
  * 
  * @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
  */
-#define AGREE_TO_TERMS false
+#define AGREE_TO_TERMS true
 
 //SoftwareSerial TX on pin 3 -> connect to GREY wire on the Jetson Bolt Pro
 SoftwareSerial mySerial(2, 3); // RX, TX
